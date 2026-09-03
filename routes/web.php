0001596ab8f24ee2ab6959eb3dd3eadd91f6bb7f
Hariom Birla <infoharry99@gmail.com>
2026-09-03 13:36:28 +0530

    Add numeric constraints to route parameters in web.php

--- routes/web.php
+++ routes/web.php
@@ -28,22 +28,22 @@ Route::middleware('auth')->group(function (): void {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-        // Multiple Email Accounts Management (Static routes MUST come before {id} wildcard routes)
+        // Multiple Email Accounts Management
         Route::post('/accounts/test-connection', [EmailAccountController::class, 'testConnection'])->name('accounts.test-connection');
         Route::get('/accounts', [EmailAccountController::class, 'index'])->name('accounts.index');
         Route::get('/accounts/create', [EmailAccountController::class, 'create'])->name('accounts.create');
         Route::post('/accounts', [EmailAccountController::class, 'store'])->name('accounts.store');
-        Route::get('/accounts/{id}/edit', [EmailAccountController::class, 'edit'])->name('accounts.edit');
-        Route::put('/accounts/{id}', [EmailAccountController::class, 'update'])->name('accounts.update');
-        Route::delete('/accounts/{id}', [EmailAccountController::class, 'destroy'])->name('accounts.destroy');
+        Route::get('/accounts/{id}/edit', [EmailAccountController::class, 'edit'])->name('accounts.edit')->whereNumber('id');
+        Route::put('/accounts/{id}', [EmailAccountController::class, 'update'])->name('accounts.update')->whereNumber('id');
+        Route::delete('/accounts/{id}', [EmailAccountController::class, 'destroy'])->name('accounts.destroy')->whereNumber('id');
 
         // Leads Management
         Route::get('/leads', [LeadController::class, 'index'])->name('leads.index');
-        Route::get('/leads/{id}', [LeadController::class, 'show'])->name('leads.show');
-        Route::patch('/leads/{id}/status', [LeadController::class, 'updateStatus'])->name('leads.update-status');
-        Route::patch('/leads/{id}/assign', [LeadController::class, 'assign'])->name('leads.assign');
-        Route::post('/leads/{id}/notes', [LeadController::class, 'addNote'])->name('leads.add-note');
-        Route::patch('/leads/{id}/extracted', [LeadController::class, 'updateExtracted'])->name('leads.update-extracted');
+        Route::get('/leads/{id}', [LeadController::class, 'show'])->name('leads.show')->whereNumber('id');
+        Route::patch('/leads/{id}/status', [LeadController::class, 'updateStatus'])->name('leads.update-status')->whereNumber('id');
+        Route::patch('/leads/{id}/assign', [LeadController::class, 'assign'])->name('leads.assign')->whereNumber('id');
+        Route::post('/leads/{id}/notes', [LeadController::class, 'addNote'])->name('leads.add-note')->whereNumber('id');
+        Route::patch('/leads/{id}/extracted', [LeadController::class, 'updateExtracted'])->name('leads.update-extracted')->whereNumber('id');
 
         // Email Synchronization & Logs
         Route::post('/sync', [EmailSyncController::class, 'sync'])->name('sync');
@@ -53,9 +53,9 @@ Route::middleware('auth')->group(function (): void {
         Route::get('/users', [UserManagementController::class, 'index'])->name('users.index');
         Route::get('/users/create', [UserManagementController::class, 'create'])->name('users.create');
         Route::post('/users', [UserManagementController::class, 'store'])->name('users.store');
-        Route::get('/users/{id}/edit', [UserManagementController::class, 'edit'])->name('users.edit');
-        Route::put('/users/{id}', [UserManagementController::class, 'update'])->name('users.update');
-        Route::delete('/users/{id}', [UserManagementController::class, 'destroy'])->name('users.destroy');
+        Route::get('/users/{id}/edit', [UserManagementController::class, 'edit'])->name('users.edit')->whereNumber('id');
+        Route::put('/users/{id}', [UserManagementController::class, 'update'])->name('users.update')->whereNumber('id');
+        Route::delete('/users/{id}', [UserManagementController::class, 'destroy'])->name('users.destroy')->whereNumber('id');
 
         // Profile & Change Password
         Route::get('/profile/change-password', [AuthController::class, 'showChangePassword'])->name('profile.change-password');
